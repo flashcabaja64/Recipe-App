4777ade752df11df80eb5a33dbeca0404ee26db5
Hubert Yang <offspring7890@yahoo.com>
2020-03-15 21:35:01 -0700

    added css and models for shopping-list and recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Grilled Chicken', 'Raw Chicken', 'https://www.skinnytaste.com/wp-content/uploads/2019/06/How-To-Make-Juicy-Air-Fryer-Chicken-Breasts-7.jpg')
+  ];
 
   constructor() { }
 
